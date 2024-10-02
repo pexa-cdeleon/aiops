38961d1538888e6d66ac264304ec1923f8000b2c
claudedenson <165854103+claudedenson@users.noreply.github.com>
2024-10-03 07:44:19 +1000

    Update pvt.py

--- scripts/pvt.py
+++ scripts/pvt.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3
 
+from selenium import webdriver
+
+browser = webdriver.Chrome()
+browser.get('http://www.yahoo.com')
+assert 'Yahoo' in browser.title
+
 print("Hello Python")
