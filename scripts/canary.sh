975275a0288631cb02353a93ae5be2b0bd282556
claudedenson <165854103+claudedenson@users.noreply.github.com>
2024-10-03 08:34:31 +1000

    Update canary.sh

--- scripts/canary.sh
+++ scripts/canary.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
+set -e
+
+log() {printf "INFO: ${1}\n";}
+loge() {printf "ERROR: ${1}\n";}
+
+#------------------------------------------------------------------
+log "Create Python virtual environment"
 
 echo 'Hello PEXA'
+#Check if virtual env is installed
+check=$(pip list | grep virtualenv)
+if [[ -z ${check} ]]; then
+    log "virtualenv not installed, installing..."
+    pip install virtualenv
+else
+    log "virtualenv already installed"
+fi
+
 
 `python3 aiops/scripts/pvt.py`
