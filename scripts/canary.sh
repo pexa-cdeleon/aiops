1e3d48dbc3457a71ec2be9622e08167874835b3b
claudedenson <165854103+claudedenson@users.noreply.github.com>
2024-10-03 09:00:18 +1000

    Update canary.sh

--- scripts/canary.sh
+++ scripts/canary.sh
@@ -3,16 +3,6 @@ set -e
 
 #------------------------------------------------------------------
 echo "Create Python virtual environment"
-
-echo 'Hello PEXA'
-#Check if virtual env is installed
-check=$(pip list | grep virtualenv)
-if [[ -z ${check} ]]; then
-    echo "virtualenv not installed, installing..."
-    pip install virtualenv
-else
-    echo "virtualenv already installed"
-fi
-
+pip install virtualenv
 
 `python3 aiops/scripts/pvt.py`
