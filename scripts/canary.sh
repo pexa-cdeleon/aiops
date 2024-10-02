c645e0626455c866b607acb0c2241b24ee266883
claudedenson <165854103+claudedenson@users.noreply.github.com>
2024-10-03 08:37:08 +1000

    Update canary.sh

--- scripts/canary.sh
+++ scripts/canary.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-log() {printf "INFO: ${1}\n";}
-loge() {printf "ERROR: ${1}\n";}
+log() { printf "INFO: ${1}\n"; }
+loge() { printf "ERROR: ${1}\n"; }
 
 #------------------------------------------------------------------
 log "Create Python virtual environment"
