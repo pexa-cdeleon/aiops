4d6142179e358b30e719bf1ac55aca30b9ab20ad
claudedenson <165854103+claudedenson@users.noreply.github.com>
2024-10-03 13:46:33 +1000

    Update pvt.py

--- scripts/pvt.py
+++ scripts/pvt.py
@@ -1,24 +1,56 @@
-#!/usr/bin/env python3
-
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import Select
-from selenium import webdriver
+import asyncio
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
+from aws_synthetics.selenium import synthetics_webdriver as syn_webdriver
+from aws_synthetics.common import synthetics_logger as logger, synthetics_configuration
 
-service = Service(r"/usr/bin/chromedriver")
-options = webdriver.ChromeOptions()
-options.add_argument("--no-sandbox")
-options.add_argument("--headless=new")
+TIMEOUT = 10
 
-driver = webdriver.Chrome(service=service,options=options)
-driver.get("https:/www.pexa.com.au")
+async def main():
+    url = "https://www.pexa.com.au/"
+    browser = syn_webdriver.Chrome()
 
-WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="__next"]/div/div[1]/div[1]/div[1]/div/div/div/div/div/div[2]/div[4]/a'))).click()
-WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="__next"]/div/div[1]/div[1]/div[1]/div/div/div/div/div/div[2]/div[4]/div/div/div/div/div/div/div[2]/ul/li[8]/a'))).click()
 
-print(driver.title)
+    # Set synthetics configuration
+    synthetics_configuration.set_config({
+       "screenshot_on_step_start" : True,
+       "screenshot_on_step_success": True,
+       "screenshot_on_step_failure": True
+    });
 
-driver.quit()
-print('Done')
+
+    def navigate_to_page():
+        browser.implicitly_wait(TIMEOUT)
+        browser.get(url)
+
+    await syn_webdriver.execute_step("navigateToUrl", navigate_to_page)
+
+    # Execute customer steps
+    def customer_actions_1():
+        browser.find_element(By.XPATH, "//*[@id='__next']/div/div[1]/div[1]/div[1]/div/div/div/div/div/div[2]/div[4]/a").click()
+
+    await syn_webdriver.execute_step('click', customer_actions_1)
+
+    def customer_actions_2():
+        browser.find_element(By.XPATH, "//*[@id='__next']/div/div[1]/div[1]/div[1]/div/div/div/div/div/div[2]/div[4]/div/div/div/div/div/div/div[2]/ul/li[8]/a")
+
+    await syn_webdriver.execute_step('verifySelector', customer_actions_2)
+
+    def customer_actions_3():
+        browser.find_element(By.XPATH, "//*[@id='__next']/div/div[1]/div[1]/div[1]/div/div/div/div/div/div[2]/div[4]/div/div/div/div/div/div/div[2]/ul/li[8]/a").click()
+
+    await syn_webdriver.execute_step('click', customer_actions_3)
+
+    def customer_actions_4():
+        browser.find_element(By.XPATH, "//*[@id='__next']/div/div[2]/div[2]/div/div/div/div/div/div[2]/form/div[8]/div/div/div/button")
+
+    await syn_webdriver.execute_step('verifySelector', customer_actions_4)
+
+
+
+    logger.info("Canary successfully executed.")
+
+
+async def handler(event, context):
+    # user defined log statements using synthetics_logger
+    logger.info("Selenium Python workflow canary.")
+    return await main()
