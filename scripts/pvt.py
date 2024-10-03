7ccd3b45a2805234a92c2474b18886f7eea9a8f1
pexa-cdeleon <113644327+pexa-cdeleon@users.noreply.github.com>
2024-10-03 10:50:31 +1000

    Update pvt.py

--- scripts/pvt.py
+++ scripts/pvt.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
@@ -11,6 +12,8 @@ options.add_argument("--headless=new")
 
 driver = webdriver.Chrome(service=service,options=options)
 driver.get("https://workspaces.pexa.com.au/pexa_web/login.html")
+
+WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.ID, 'username')))
 element = driver.find_element(By.ID, "sign-in-link")
 assert element.is_enabled()
 print(element.is_enabled())
