5a45bcca447589431b5331537837bd5159f8627e
claudedenson <165854103+claudedenson@users.noreply.github.com>
2024-10-03 14:04:10 +1000

    Revert

--- scripts/pvt.py
+++ scripts/pvt.py
@@ -3,41 +3,22 @@
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
+from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import TimeoutException
-from selenium.common.exceptions import NoSuchElementException
-import selenium.common.exceptions
-import boto3
-import base64
-import json
-from botocore.exceptions import ClientError
-from aws_synthetics.selenium import synthetics_webdriver as webdriver
-from aws_synthetics.common import synthetics_logger as logger
-from aws_synthetics.common import synthetics_configuration
+from selenium.webdriver.chrome.service import Service
 
-TIMEOUT = 5
-url = "https://workspaces.pexa.com.au/pexa_web/login.html"
+service = Service(r"/usr/bin/chromedriver")
+options = webdriver.ChromeOptions()
+options.add_argument("--no-sandbox")
+options.add_argument("--headless=new")
 
-async def main():
-    browser = webdriver.Chrome()
+driver = webdriver.Chrome(service=service,options=options)
+driver.get("https://workspaces.pexa.com.au/pexa_web/login.html")
 
-    synthetics_configuration.set_config(
-        {
-            "screenshot_on_step_start": False,
-            "screenshot_on_step_success": True,
-            "screenshot_on_step_failure": True
-        }
-    )
+WebDriverWait(webdriver, 10).until(EC.presence_of_element_located((By.ID, 'username')))
 
-    logger.info("------------------Production Verification Tests Started------------------")
-    
-    def navigate_to_home():
-        browser.implicitly_wait(TIMEOUT)
-        browser.get(url)
-    await webdriver.execute_step("Navigate to PEXA Exchange", navigate_to_home)
+driver.find_element_by_id('username')
 
+print(driver.title)
 
-    logger.info("------------------Production Verification Tests Completed------------------")
-    
-async def handler(event, context):
-    return await main()
+driver.quit()
