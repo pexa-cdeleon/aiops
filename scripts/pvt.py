c1202dceeb2cb078c9e2f3d3ea475eab731ffe34
claudedenson <165854103+claudedenson@users.noreply.github.com>
2024-10-03 14:00:24 +1000

    Update pvt.py

--- scripts/pvt.py
+++ scripts/pvt.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
