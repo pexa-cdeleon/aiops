e76f5dca17bc79ce2acef8d0f65b3286bfc61da0
pexa-cdeleon <113644327+pexa-cdeleon@users.noreply.github.com>
2024-12-19 09:04:59 +1100

    Delete scripts directory

--- scripts/canary.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-#------------------------------------------------------------------
-echo "Create Python virtual environment"
-pip install virtualenv
-
-`python3 pvt.py`
